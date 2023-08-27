2178662e5afab0891b0e6d702df08987a182298b
annandrievska <annandrievska1@gmail.com>
2023-08-27 18:31:30 +0300

    js alerts

---
+++ jscript.js
@@ -0,0 +1,18 @@
+let tip = "You can ignore single and double quotes like this:\\\' \\\' \\\" \\\"  "
+console.log (tip)
+
+alert('Please enter your data');
+
+let userName = prompt("Enter your name", 'Name');
+alert('Your name is: ' + userName);
+
+let surname = prompt("Enter your surname", 'Surname');
+alert('Your full name is: ' + userName + ' ' + surname);
+
+let age = prompt("How old are you?", 'Age');
+alert('Your full name is: ' + userName + ' ' + surname + ', your age is: ' + age);
+
+let access = confirm('Are you admin?');
+alert('Your full name is: ' + userName + ' ' + surname + ', your age is: ' + age + ', admin status: ' + access);
+
+
