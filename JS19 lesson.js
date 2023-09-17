9aa2b3e2d92dc7910c7900e2b7dd41e5d1d02e40
annandrievska <annandrievska1@gmail.com>
2023-09-17 15:41:32 +0300

    JS Lesson 19 commit

---
+++ JS19 lesson.js
@@ -0,0 +1,35 @@
+// Завдання 1
+
+// let firstSalary1 = Number(prompt("Enter first month salary", ''));
+// let secondSalary1 = Number(prompt("Enter second month salary", ''));
+// let sum1 = firstSalary1 + secondSalary1;
+// alert(`Your salary for 2 months is: ${sum1} $`);
+
+// Завдання 2
+
+// let firstSalary2 = Number(prompt("Enter first month salary", ''));
+// let secondSalary2 = Number(prompt("Enter second month salary", ''));
+// let sum2 = firstSalary2 + secondSalary2;
+// let bonus = sum2 + 1;
+// alert(`You have an extra bonus! Your total salary was: ${sum2} $ It has increased by 1$ and now it is: ${bonus} $`);
+
+// Завдання 3
+
+// let firstSalary3 = Number(prompt("Enter first month salary", ''));
+// let secondSalary3 = Number(prompt("Enter second month salary", ''));
+// let sum3 = firstSalary3 + secondSalary3;
+// let bonus3 = sum3 + 1;
+// let total = bonus3 >= 2000;
+// alert(`You have an extra bonus! Your total salary was: ${sum3} $ It has increased by 1$ and now it is: ${bonus3} $`);
+// alert(`Will you work next? ${total}`);
+
+// Завдання 4
+
+let firstSalary4 = Number(prompt("Enter first month salary", ''));
+let secondSalary4 = Number(prompt("Enter second month salary", ''));
+let sum4 = firstSalary4 + secondSalary4;
+let bonus4 = sum4 + 1;
+let total = bonus4 >= 2000;
+let work = total ? "I am ready to work" : "I am out";
+alert(`You have extra bonus! Your total salary was: ${sum4} $ It is increased by 1$ and now it is: ${bonus4} $`);
+alert(`${work}`);
